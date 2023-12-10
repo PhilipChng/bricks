0e9b0449345e7689161778ca18910d7b482fa7f2
Philip Chng <pczn0327@gmail.com>
2023-12-10 20:40:23 +0800

    Project initialized with feature brick

---
+++ feature/__brick__/{{feature_name}}/view/{{feature_name}}_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+@RoutePage()
+class {{feature_name.pascalCase()}}Page extends StatelessWidget {
+  const {{feature_name.pascalCase()}}Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => {{feature_name.pascalCase()}}Bloc(),
+      child: const {{feature_name.pascalCase()}}View(),
+    );
+  }
+}
+
+class {{feature_name.pascalCase()}}View extends StatelessWidget {
+  const {{feature_name.pascalCase()}}View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('{{feature_name.pascalCase()}} View'),
+      ),
+    );
+  }
---
+++ feature/__brick__/{{feature_name}}/{{feature_name}}.dart
@@ -0,0 +1,2 @@
+export 'bloc/{{feature_name}}_bloc.dart';
+export 'view/{{feature_name}}_page.dart';
