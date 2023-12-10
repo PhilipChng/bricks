abee622205b9298caa47044f1681d92faae9ec1d
Philip Chng <pczn0327@gmail.com>
2023-12-10 20:50:05 +0800

    [Feature] Migrate output to features folder

--- feature/__brick__/lib/features/{{feature_name}}/bloc/{{feature_name}}_bloc.dart
+++ feature/__brick__/lib/features/{{feature_name}}/bloc/{{feature_name}}_bloc.dart
@@ -5,9 +5,5 @@ part '{{feature_name}}_event.dart';
 part '{{feature_name}}_state.dart';
 
 class {{feature_name.pascalCase()}}Bloc extends Bloc<{{feature_name.pascalCase()}}Event, {{feature_name.pascalCase()}}State> {
-  {{feature_name.pascalCase()}}Bloc() : super({{feature_name.pascalCase()}}Initial()) {
-    on<{{feature_name.pascalCase()}}Event>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
+  {{feature_name.pascalCase()}}Bloc() : super({{feature_name.pascalCase()}}Initial());
 }
